2f788997a4b23f97b146ae7c86212317772e7a24
Simon Foster <funkysi1701@gmail.com>
2025-12-05 16:18:03 +0000

    tweak function

---
+++ FestiveTechCalendar2025/ExamplePlugin.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace FestiveTechCalendar2025;
+
+public class ExamplePlugin
+{
+    [KernelFunction]
+    public int GenerateHash(string input)
+    {
+        var hash = input.GetHashCode();
+
+        return hash > 0 ? 0 : 1;
+    }
+}
